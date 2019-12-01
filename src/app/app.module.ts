ab3e8d0009fb37fd01067a903c5ca02f134b1a3e
Vicente Mata <matavic25@gmail.com>
2019-12-01 11:12:28 -0400

    Added Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {NgbCollapseModule} from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbCollapseModule
   ],
   providers: [],
   bootstrap: [AppComponent]

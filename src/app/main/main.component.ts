3db46a2a59255c4797aa9c4f05bac3bad6460044
Vicente Mata <matavic25@gmail.com>
2019-12-10 21:58:17 -0400

    Added TBA message when no award data is available

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -9,10 +9,15 @@ import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 })
 export class MainComponent implements OnInit {
 
+  // images = [
+  //   '../assets/img/ceremony1.jpg',
+  //   '../assets/img/ceremony2.jpg',
+  //   '../assets/img/ceremony3.jpg',
+  // ];
   images = [
-    '../assets/img/ceremony1.jpg',
-    '../assets/img/ceremony2.jpg',
-    '../assets/img/ceremony3.jpg',
+    'https://res.cloudinary.com/dw5ahwj7g/image/upload/v1576001022/filmawardsseason/ceremony1_hitlpn.jpg',
+    'https://res.cloudinary.com/dw5ahwj7g/image/upload/v1576001023/filmawardsseason/ceremony2_pxmyb2.jpg',
+    'https://res.cloudinary.com/dw5ahwj7g/image/upload/v1576001024/filmawardsseason/ceremony3_w9wtai.jpg',
   ];
 
   constructor(config: NgbCarouselConfig) {
